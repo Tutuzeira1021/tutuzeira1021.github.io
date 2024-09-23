d5ac41425d145768b354da5431c24e8cdb5a6149
Tutuzeira1021 <167903753+Tutuzeira1021@users.noreply.github.com>
2024-09-23 10:25:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
- if( navigator.userAgent.match(/Android/i) 
- || navigator.userAgent.match(/webOS/i) 
- || navigator.userAgent.match(/iPhone/i) 
- || navigator.userAgent.match(/iPad/i) 
- || navigator.userAgent.match(/iPod/i) 
- || navigator.userAgent.match(/BlackBerry/i) 
- || navigator.userAgent.match(/Windows Phone/i) 
- ){ 
-     window.location.href("https://tutuzeira1021.github.io/mobile.index.html")
- }
+redi_width = 425;
+if(window.innerWidth < redi_width) {
+    window.location.replace(tutuzeira1021.github.io/mobile/index.html);
+}
